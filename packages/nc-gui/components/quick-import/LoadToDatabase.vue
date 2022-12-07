00ec8107c70e6a16566b36c83ae8f0dc90b08094
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-12-07 13:39:04 +0800

    feat(nc-gui): add imported tables list

--- packages/nc-gui/components/quick-import/LoadToDatabase.vue
+++ packages/nc-gui/components/quick-import/LoadToDatabase.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+import { useQuickImportStoreOrThrow } from '#imports'
+
+const { importedTables } = useQuickImportStoreOrThrow()!
+
+const selectedTables = ref<Record<string, boolean>>({})
+
+importedTables.value.forEach((t) => {
+  selectedTables.value[t.id!] = false
+})
+
 function append() {
   // TODO:
 }
@@ -13,7 +23,7 @@ function overwrite() {
 </script>
 
 <template>
-  <a-row :gutter="16">
+  <a-row class="p-[20px]" :gutter="16">
     <a-col :span="8">
       <a-card :title="$t('activity.quickImport.loadToDataBase.append')" class="nc-import-action-button" @click="append">
         <p>{{ $t('activity.quickImport.loadToDataBase.appendDesc') }}</p>
@@ -28,6 +38,31 @@ function overwrite() {
       <a-card :title="$t('activity.quickImport.loadToDataBase.overwrite')" class="nc-import-action-button" @click="overwrite">
         <p>{{ $t('activity.quickImport.loadToDataBase.overwriteDesc') }}</p>
       </a-card>
+    </a-col>
+  </a-row>
+  <a-row class="p-[20px]">
+    <a-col :span="24">
+      <a-list item-layout="horizontal" :data-source="importedTables">
+        <template #renderItem="{ item, index }">
+          <a-list-item class="cursor-pointer">
+            <a-list-item-meta>
+              <template #title>
+                <div class="flex items-center py-1">
+                  <MdiTableLarge class="text-sm text-primary" />
+                  {{ item.title }}
+                </div>
+              </template>
+              <template #avatar>
+                <a-checkbox v-model:checked="selectedTables[item.id]" />
+              </template>
+              <template #description>
+                Created At: {{ item.created_at }} <br />
+                Last Modified At: {{ item.updated_at }}
+              </template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
     </a-col>
   </a-row>
 </template>
