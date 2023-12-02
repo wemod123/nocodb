6c6d8ebc47fcb06c84ec5be70a9a6dbd5fcc47a9
Jason <wemod@163.com>
2023-12-02 10:59:03 +0800

    build-x-7

---
+++ packages/nocodb/build-utils/sync.js
@@ -0,0 +1,12 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const guiLibSrcDir = path.join(__dirname, '../../nc-lib-gui');
+const guiLibDestDir = path.join(__dirname, '../nc-lib-gui');
+fs.removeSync(guiLibDestDir);
+fs.copySync(guiLibSrcDir, guiLibDestDir);
+
+const sdkSrcDir = path.join(__dirname, '../../nocodb-sdk');
+const sdkDestDir = path.join(__dirname, '../nocodb-sdk');
+fs.removeSync(sdkDestDir)
+fs.copySync(sdkSrcDir, sdkDestDir)
