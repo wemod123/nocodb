929f49be14ede745c95c8a3c7bd944d321f405c6
Jason <wemod@163.com>
2023-12-02 16:17:19 +0800

    build-x-10

--- packages/nocodb/build-utils/sync.js
+++ packages/nocodb/build-utils/sync.js
@@ -1,12 +1,35 @@
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
+
+const removeDir = promisify(fs.rmdir);
+const copyFile = promisify(fs.copyFile);
+const mkdir = promisify(fs.mkdir);
 
 const guiLibSrcDir = path.join(__dirname, '../../nc-lib-gui');
 const guiLibDestDir = path.join(__dirname, '../nc-lib-gui');
-fs.removeSync(guiLibDestDir);
-fs.copySync(guiLibSrcDir, guiLibDestDir);
 
 const sdkSrcDir = path.join(__dirname, '../../nocodb-sdk');
 const sdkDestDir = path.join(__dirname, '../nocodb-sdk');
-fs.removeSync(sdkDestDir)
-fs.copySync(sdkSrcDir, sdkDestDir)
+
+async function copyAndRemoveDirs(srcDir, destDir) {
+  try {
+    await removeDir(destDir, { recursive: true });
+    await mkdir(destDir, { recursive: true });
+
+    const files = await fs.promises.readdir(srcDir);
+
+    for (const file of files) {
+      const srcFile = path.join(srcDir, file);
+      const destFile = path.join(destDir, file);
+      await copyFile(srcFile, destFile);
+    }
+
+    console.log(`Copied from ${srcDir} to ${destDir}`);
+  } catch (err) {
+    console.error(`Error copying from ${srcDir} to ${destDir}: ${err}`);
+  }
+}
+
+copyAndRemoveDirs(guiLibSrcDir, guiLibDestDir);
+copyAndRemoveDirs(sdkSrcDir, sdkDestDir);
